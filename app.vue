e10438d2a3a40194f47ede2188b6b62ef2d2d6ba
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-12-25 23:18:37 +0100

    Add tailwind css

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-full p-3 bg-slate-300">
+    <NuxtPage />
   </div>
 </template>
+<style lang="scss">
+html, body, #__nuxt {
+  height: 100%;
+}
+</style>
