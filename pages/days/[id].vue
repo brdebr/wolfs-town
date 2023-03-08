c4c4bf4b279d6538e040795f854e27a3d5c548dd
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2023-03-08 22:06:11 +0100

    Lay out some things in the days page

--- pages/days/[id].vue
+++ pages/days/[id].vue
@@ -17,12 +17,95 @@
     </template>
     <template #default>
       <div v-if="day" class="mt-3 flex flex-col gap-3">
-        <div v-for="row in day.playerRows" :key="row.player" class="flex">
+        <div class="grid grid-cols-4 gap-5 text-center underline">
+          <div>
+            Player name
+          </div>
+          <div>
+            Defends
+          </div>
+          <div>
+            Suspects
+          </div>
+          <div>
+            Roles guesses
+          </div>
+        </div>
+        <div v-for="row in day.playerRows" :key="row.player" class="grid grid-cols-1 lg:grid-cols-4 items-center gap-12 lg:gap-5 focus-within:z-10 pb-3 border-b border-white border-opacity-40 last:border-0">
           <div class="">
             <BasePlayerInput readonly :player="gameStore.getPlayerById(row.player)" />
           </div>
-          <div>
-            
+          <div class="flex flex-col gap-3">
+            <BasePlayerInput readonly :player="gameStore.getPlayerById(row.player)" />
+            <BasePlayerInput readonly :player="gameStore.getPlayerById(row.player)" />
+            <BasePlayerInput readonly :player="gameStore.getPlayerById(row.player)" />
+          </div>
+          <div class="flex flex-col gap-3">
+            <BasePlayerInput readonly :player="gameStore.getPlayerById(row.player)" />
+            <BasePlayerInput readonly :player="gameStore.getPlayerById(row.player)" />
+            <BasePlayerInput readonly :player="gameStore.getPlayerById(row.player)" />
+          </div>
+          <div class="flex flex-col gap-3">
+            <BaseSelect
+              :items="possibleRoles"
+              :displayValueFn="displayFn"
+              valueKey="name"
+              textKey="name"
+              key-prop="name"
+              v-model="selected"
+              v-model:query="query"
+              @selected="addRole"
+              :exclude-search-keys="['alignment']"
+            >
+              <template #item="{ item }">
+                <span class="mr-4">
+                  {{ item.emoji }}
+                </span>
+                <span>
+                  {{ item.displayName }}
+                </span>
+              </template>
+            </BaseSelect>
+            <BaseSelect
+              :items="possibleRoles"
+              :displayValueFn="displayFn"
+              valueKey="name"
+              textKey="name"
+              key-prop="name"
+              v-model="selected"
+              v-model:query="query"
+              @selected="addRole"
+              :exclude-search-keys="['alignment']"
+            >
+              <template #item="{ item }">
+                <span class="mr-4">
+                  {{ item.emoji }}
+                </span>
+                <span>
+                  {{ item.displayName }}
+                </span>
+              </template>
+            </BaseSelect>
+            <BaseSelect
+              :items="possibleRoles"
+              :displayValueFn="displayFn"
+              valueKey="name"
+              textKey="name"
+              key-prop="name"
+              v-model="selected"
+              v-model:query="query"
+              @selected="addRole"
+              :exclude-search-keys="['alignment']"
+            >
+              <template #item="{ item }">
+                <span class="mr-4">
+                  {{ item.emoji }}
+                </span>
+                <span>
+                  {{ item.displayName }}
+                </span>
+              </template>
+            </BaseSelect>
           </div>
         </div>
       </div>
@@ -36,6 +119,26 @@
 <script setup lang="ts">
 const { params: { id } } = useRoute();
 const idParam = computed(() => [id].flat()[0]);
+
+// Roles
+const rolesStore = useRolesStore();
+const { roles } = storeToRefs(rolesStore);
+const displayFn = (role: unknown) => {
+  const roleObj = role as Role;
+  if (!roleObj) {
+    return '';
+  }
+  return `${roleObj.emoji} - ${roleObj.displayName || ''}`;
+};
+
+const selected = ref<Role | null>(null);
+const query = ref('');
+
+const addRole = (role: Role | null) => {
+  if (!role) {
+    return;
+  }
+};
 
 const day = computed(() => {
   if (!gameStore.game.days) {
